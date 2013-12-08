e4649ee01091bd572a27fb6a993c55b288a1dd6b
Cees-Jan <ceesjank@gmail.com>
2013-12-08 14:29:23 +0100

    Using test plugin for plugin testing

--- Test/Case/Lib/Twig_Loader_CakephpTest.php
+++ Test/Case/Lib/Twig_Loader_CakephpTest.php
@@ -7,13 +7,24 @@ class Twig_Loader_CakephpTest extends CakeTestCase {
 	public function setUp() {
 		parent::setUp();
 
+		App::build(array(
+			'Plugin' => array(App::pluginPath('TwigView') . 'Test' . DS . 'test_app' . DS . 'Plugin' . DS )
+		));
+		CakePlugin::load('TestTwigView');
+
 		$this->Loader = new Twig_Loader_Cakephp();
 	}
 
 	public function tearDown() {
 		unset($this->Loader);
 
+		CakePlugin::unload('TestTwigView');
+
 		parent::tearDown();
+	}
+
+	public function testGetSource() {
+
 	}
 
 	public function testGetCacheKeyNoPlugin() {
@@ -21,7 +32,7 @@ class Twig_Loader_CakephpTest extends CakeTestCase {
 	}
 
 	public function testGetCacheKeyPlugin() {
-		$this->assertSame(APP . 'Plugin/TwigView/View/layout.tpl', $this->Loader->getCacheKey('TwigView.layout'));
+		$this->assertSame(APP . 'Plugin/TwigView/Test/test_app/Plugin/TestTwigView/View/twig.tpl', $this->Loader->getCacheKey('TestTwigView.twig'));
 	}
 
 }
