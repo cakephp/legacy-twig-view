cbc2a1ce87be12452bae5888a8ed35ffcbea05aa
Cees-Jan <ceesjank@gmail.com>
2013-12-12 22:32:20 +0100

    Docblocks for the Inflector extension

--- Lib/Twig_Extension_Inflector.php
+++ Lib/Twig_Extension_Inflector.php
@@ -1,30 +1,84 @@
 <?php
 class Cake_Inflector_Filters
 {
+
+/**
+ * Inflector::camelize wrapper
+ * @param $str
+ * @return string
+ */
     public static function camelize($str) {
         return Inflector::camelize($str);
     }
+
+/**
+ * Inflector::classify wrapper
+ * @param $str
+ * @return string
+ */
     public static function classify($str) {
         return Inflector::classify($str);
     }
+
+/**
+ * Inflector::humanize wrapper
+ * @param $str
+ * @return string
+ */
     public static function humanize($str) {
         return Inflector::humanize($str);
     }
+
+/**
+ * Inflector::pluralize wrapper
+ * @param $str
+ * @return string
+ */
     public static function pluralize($str) {
         return Inflector::pluralize($str);
     }
+
+/**
+ * Inflector::singularize wrapper
+ * @param $str
+ * @return string
+ */
     public static function singularize($str) {
         return Inflector::singularize($str);
     }
+
+/**
+ * Inflector::slug wrapper
+ * @param $str
+ * @return string
+ */
     public static function slug($str) {
-        return Inflector::slug($str); // Might have to get a betterone for this
+        return Inflector::slug($str);
     }
+
+/**
+ * Inflector::tableize wrapper
+ * @param $str
+ * @return string
+ */
     public static function tableize($str) {
         return Inflector::tableize($str);
     }
+
+/**
+ * Inflector::underscore wrapper
+ * @param $str
+ * @return string
+ */
     public static function underscore($str) {
         return Inflector::underscore($str);
     }
+
+/**
+ * Inflector::variable wrapper
+ * @param $str
+ * @return string
+ */
     public static function variable($str) {
         return Inflector::variable($str);
     }
