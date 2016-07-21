d8e48780f7ae80c1da041d674841b78e82bafa01
Yves P <havokinspiration@gmail.com>
2016-07-21 14:09:33 +0200

    Fix indentation

--- tests/View/TwigViewTest.php
+++ tests/View/TwigViewTest.php
@@ -140,10 +140,10 @@ class TwigViewTest extends TestCase
 			]
 		);
 
-        $view = new TwigView(Phake::mock('Cake\Network\Request'), Phake::mock('Cake\Network\Response'), Phake::mock('Cake\Event\EventManager'));
-        $view->TestHelper = 'foo:bar';
-        $view->helpers = $helpersArray;
-        $view->loadHelper('TestSecond');
+		$view = new TwigView(Phake::mock('Cake\Network\Request'), Phake::mock('Cake\Network\Response'), Phake::mock('Cake\Event\EventManager'));
+		$view->TestHelper = 'foo:bar';
+		$view->helpers = $helpersArray;
+		$view->loadHelper('TestSecond');
 		$view->TestSecond = 'bar:foo';
 
 		self::getMethod('generateHelperList')->invoke($view);
