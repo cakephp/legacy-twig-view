34a7e6fd1c5a0096d7d3dde4c734273dbdc1c98e
ADmad <admad.coder@gmail.com>
2019-03-12 19:41:07 +0530

    Update use of removed method.

--- tests/Lib/ScannerTest.php
+++ tests/Lib/ScannerTest.php
@@ -10,7 +10,6 @@
 
 namespace WyriHaximus\CakePHP\Tests\TwigView\Lib;
 
-use Cake\Core\Plugin;
 use Cake\Routing\Router;
 use WyriHaximus\CakePHP\Tests\TwigView\TestCase;
 use WyriHaximus\TwigView\Lib\Scanner;
@@ -31,7 +30,7 @@ class ScannerTest extends TestCase
 
     public function tearDown()
     {
-        Plugin::unload('TestTwigView');
+        $this->removePlugins(['TestTwigView']);
 
         parent::tearDown();
     }
