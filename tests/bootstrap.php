09b0a825dca6314c25bfb75866ecc890a0be7d8e
ADmad <admad.coder@gmail.com>
2020-02-29 20:12:56 +0530

    Remove use of deprecated class.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,7 @@ use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
 use Cake\Datasource\ConnectionManager;
+use Cake\Filesystem\Filesystem;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/vendor/cakephp/cakephp/src/basics.php';
@@ -21,10 +22,10 @@ define('CACHE', sys_get_temp_dir() . '/TwigViewTmp/cache/');
 define('PLUGIN_REPO_ROOT', dirname(__DIR__) . DS);
 define('TEST_APP', PLUGIN_REPO_ROOT . 'tests/test_app' . DS);
 
-$TMP = new \Cake\Filesystem\Folder(TMP);
-$TMP->create(TMP . 'cache/models', 0777);
-$TMP->create(TMP . 'cache/persistent', 0777);
-$TMP->create(TMP . 'cache/views', 0777);
+$fs = new Filesystem();
+$fs->mkdir(TMP . 'cache/models', 0777);
+$fs->mkdir(TMP . 'cache/persistent', 0777);
+$fs->mkdir(TMP . 'cache/views', 0777);
 
 Configure::write('debug', true);
 if (!getenv('db_dsn')) {
