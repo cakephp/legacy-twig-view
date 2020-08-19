476fd22f28c3b2f242172715ff86e5f264097829
peter279k <peter279k@gmail.com>
2020-08-20 02:20:39 +0800

    Enhancements about type assetions and namespaces

--- tests/Lib/Twig/Extension/AbstractExtensionTest.php
+++ tests/Lib/Twig/Extension/AbstractExtensionTest.php
@@ -36,7 +36,7 @@ abstract class AbstractExtensionTest extends TestCase
     public function testGetTokenParsers()
     {
         $tokenParsers = $this->extension->getTokenParsers();
-        $this->assertTrue(is_array($tokenParsers));
+        $this->assertIsArray($tokenParsers);
         foreach ($tokenParsers as $tokenParser) {
             $this->assertTrue($tokenParser instanceof TokenParserInterface);
         }
@@ -45,7 +45,7 @@ abstract class AbstractExtensionTest extends TestCase
     public function testGetNodeVisitors()
     {
         $nodeVisitors = $this->extension->getNodeVisitors();
-        $this->assertTrue(is_array($nodeVisitors));
+        $this->assertIsArray($nodeVisitors);
         foreach ($nodeVisitors as $nodeVisitor) {
             $this->assertInstanceOf('Twig_NodeVisitorInterface', $nodeVisitor);
         }
@@ -54,7 +54,7 @@ abstract class AbstractExtensionTest extends TestCase
     public function testGetFilters()
     {
         $filters = $this->extension->getFilters();
-        $this->assertTrue(is_array($filters));
+        $this->assertIsArray($filters);
         foreach ($filters as $filter) {
             $this->assertInstanceOf(TwigFilter::class, $filter);
         }
